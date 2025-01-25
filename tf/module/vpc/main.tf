ba54afba63d60b49d2c9dc49f8cfd17fd1f6b4d1
gatmbarz123 <bar30301@gmail.com>
2025-01-25 21:02:04 +0200

    check if the CICD works v10.0

--- tf/module/vpc/main.tf
+++ tf/module/vpc/main.tf
@@ -61,8 +61,8 @@ resource "aws_ssm_association" "ssm-script" {
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
       "sudo aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.account_id_pull.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com >> /home/ec2-user/debug.log 2>&1",
-      "sudo docker pull ${data.aws_caller_identity.account_id_pull.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo_name}:${var.image_tag} >> /home/ec2-user/debug.log 2>&1",
-      "sudo docker run -d -e AWS_REGION=${var.aws_region} -e DYNAMODB_TABLE=${var.dynamodb_name} -p 5000:5000 ${data.aws_caller_identity.account_id_pull.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo_name}:${var.image_tag}"
+      "sudo docker pull ${data.aws_caller_identity.account_id_pull.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo_name}:v${var.image_tag} >> /home/ec2-user/debug.log 2>&1",
+      "sudo docker run -d -e AWS_REGION=${var.aws_region} -e DYNAMODB_TABLE=${var.dynamodb_name} -p 5000:5000 ${data.aws_caller_identity.account_id_pull.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo_name}:v${var.image_tag}"
     ])
   }
 }
